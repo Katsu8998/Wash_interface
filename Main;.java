d3c8d7bae5f1f8c291a252ea4a9348e4ebb6c0fd
Katsu <114673266+Katsu8998@users.noreply.github.com>
2022-11-01 14:31:15 +0900

    Update Main;.java

--- Main;.java
+++ Main;.java
@@ -4,7 +4,9 @@ public class Main {
 	    public static void main(String[] args){  
 	        System.out.println("京都店のシャツ洗濯時間");
 	        KyotoCleaningShop V = new KyotoCleaningShop();
-	       String S = V.washShirt();
+	        V.ownerName ="tanaka";
+	        V.washShirt(s);
 	     
+	 
 	    }
 	} 
