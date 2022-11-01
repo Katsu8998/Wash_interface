106fe9b864364c1ed310487b85491a92321b5f9f
Katsu <114673266+Katsu8998@users.noreply.github.com>
2022-11-01 14:36:04 +0900

    Update Main;.java

--- Main;.java
+++ Main;.java
@@ -4,7 +4,9 @@ public class Main {
 	    public static void main(String[] args){  
 	        System.out.println("京都店のシャツ洗濯時間");
 	        KyotoCleaningShop V = new KyotoCleaningShop();
+	        Shirt s = new Shirt();//クラス型をメゾット引数に用いる場合、mainクラスで希望のクラスからインスタンス化させる
 	        V.ownerName ="tanaka";
 	        V.washShirt(s);
+	     
 	    }
 	} 
